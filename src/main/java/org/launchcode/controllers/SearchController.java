4a54dd83c58df8306c06c831dfdda332e34a8ba9
Berry Burge <bjburge@hotmail.com>
2017-03-30 18:38:34 -0500

    Additions

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -14,7 +15,18 @@ import java.util.HashMap;
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController{
+        static HashMap<String, String> columnChoices = new HashMap<>();
+        private static ArrayList<HashMap<String, String>> allJobs;
+
+
+        public SearchController () {
+            columnChoices.put("core competency", "Skill");
+            columnChoices.put("employer", "Employer");
+            columnChoices.put("location", "Location");
+            columnChoices.put("position type", "Position Type");
+            columnChoices.put("all", "All");
+        }
 
     @RequestMapping(value = "")
     public String search(Model model) {
@@ -22,6 +34,46 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+        public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+            ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+
+        if(searchType.equals("all")) {
+            for (HashMap<String, String> job : JobData.findAll()) {
+                for (String key : job.keySet()) {
+                    String value = job.get(key);
+
+                    if (value.toLowerCase().contains(searchTerm.toLowerCase())) {
+                        jobs.add(job);
+                        model.addAttribute("jobs", jobs);
+                        break;
+                    }
+                }
+            }
+        }else{
+            for (HashMap<String, String> job : JobData.findAll()) {
+
+                String aValue=job.get(searchType); //
+                if (aValue.toLowerCase().contains(searchTerm.toLowerCase())){
+
+                    jobs.add(job);
+                    model.addAttribute("jobs", jobs);
+                }
+
+
+
+            }
+        }
+        return "search";
+
+
+    }
 
 }
